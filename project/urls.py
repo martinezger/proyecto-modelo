746f65cabfe67f202f829640b4e7058d59e60824
Germán Martinez <germand_m@hotmail.com>
2023-02-28 23:04:02 -0300

    Template de proyect intermedio funcional

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from SocialTravel.views import index
 
 urlpatterns = [
+    path('', index, name="index"),
     path('admin/', admin.site.urls),
 ]
+
