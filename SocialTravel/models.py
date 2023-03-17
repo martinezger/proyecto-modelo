5a6dc27f606bb8a0576721ab1dfe4e4f5bcb5e49
Germán Martinez <germand_m@hotmail.com>
2023-03-16 23:01:15 -0300

    imagenes de posts, autorizacion solo el dueno puede editar y borrar

--- SocialTravel/models.py
+++ SocialTravel/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     carousel_caption_title = models.CharField(max_length=30)
@@ -6,6 +7,13 @@ class Post(models.Model):
     heading = models.CharField(max_length=15)
     description = models.CharField(max_length=120)
     un_campo = models.CharField(max_length=10)
+    publisher = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="publisher")
+    image = models.ImageField(upload_to="posts", null=True, blank=True)
+
+    @property
+    def image_url(self):
+        return self.image.url if self.image else ''
+
 
     def __str__(self):
         return f"{self.id} - {self.carousel_caption_title}"
--- SocialTravel/views.py
+++ SocialTravel/views.py
@@ -5,7 +5,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 def index(request):
     return render(request, "SocialTravel/index.html")
@@ -15,22 +15,37 @@ class PostList(ListView):
     model = Post
     context_object_name = "posts"
 
+class PostMineList(LoginRequiredMixin, PostList):
+    
+    def get_queryset(self):
+        return Post.objects.filter(publisher=self.request.user.id).all()
+
 
 class PostDetail(DetailView):
     model = Post
     context_object_name = "post"
 
 
-class PostUpdate(LoginRequiredMixin, UpdateView):
+class PostUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     success_url = reverse_lazy("post-list")
     fields = '__all__'
 
+    def test_func(self):
+        user_id = self.request.user.id
+        post_id =  self.kwargs.get("pk")
+        return Post.objects.filter(publisher=user_id, id=post_id).exists()
 
-class PostDelete(LoginRequiredMixin, DeleteView):
+
+class PostDelete(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     context_object_name = "post"
     success_url = reverse_lazy("post-list")
+
+    def test_func(self):
+        user_id = self.request.user.id
+        post_id =  self.kwargs.get("pk")
+        return Post.objects.filter(publisher=user_id, id=post_id).exists()
 
 
 class PostCreate(LoginRequiredMixin, CreateView):
